2abacd785e6cbdbd48326eaa6bd7b6acc37d58cb
William Artero <williamwmoraes@gmail.com>
2020-10-18 20:40:25 +0200

    chore: rename template to jinja2 standard extension

--- generate.py
+++ generate.py
@@ -3,6 +3,6 @@
 import jinja2
 import yaml
 
-template = jinja2.Template(open("README.md.tmpl", 'r').read())
+template = jinja2.Template(open("README.md.j2", 'r').read())
 profileData = yaml.load(open("profile.yaml", 'r'), Loader=yaml.FullLoader)
 open("README.md",'w').write(template.render(profileData))
