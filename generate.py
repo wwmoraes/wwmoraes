c155e42f7744430f65689376a1edbed4f404fa6c
William Artero <git@artero.dev>
2022-04-23 20:18:23 +0200

    refactor: changed template from custom python to markscribe

--- generate.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import jinja2
-import yaml
-
-template = jinja2.Template(open("README.md.j2", 'r').read())
-profileData = yaml.load(open("profile.yaml", 'r'), Loader=yaml.FullLoader)
-open("README.md",'w').write(template.render(profileData))
